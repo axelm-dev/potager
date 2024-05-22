7e5c4f8d98bc93fc9d1ed76337f54c99ddc788f2
Corentin Le Bris <101640441+Cocolb22@users.noreply.github.com>
2024-05-22 16:41:31 +0200

    creation of all entity
    creation of all entity

---
+++ src/main/java/fr/mainguy/lebris/potager/entity/PlantInSquare.java
@@ -0,0 +1,32 @@
+package fr.mainguy.lebris.potager.entity;
+
+import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import java.time.LocalDate;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+public class PlantInSquare {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "square_id", nullable = false)
+    private Square square;
+
+    @ManyToOne
+    @JoinColumn(name = "plant_id", nullable = false)
+    private Plant plant;
+
+    private int quantity;
+    private LocalDate plantDate;
+    private LocalDate harvestDate;
+}
