167203931c1947a2c18df0c84b9d824ef279d31b
axelm-dev <axel.mainguy2022@campus-eni.fr>
2024-05-23 10:08:01 +0200

    service & implementations

--- src/main/java/fr/mainguy/lebris/potager/service/PottageService.java
+++ src/main/java/fr/mainguy/lebris/potager/service/PottageService.java
@@ -1,13 +1,25 @@
 package fr.mainguy.lebris.potager.service;
 
 import fr.mainguy.lebris.potager.entity.Plant;
+import fr.mainguy.lebris.potager.entity.Pottage;
 
 import java.util.List;
 
 public interface PottageService {
 
+    public void createPottage();
+
+    public void deletePottage();
+
+    public void updatePottage();
+
+    public void getPottage();
+
+    public List<Pottage> getAllPottage();
+
     public void addPlantInPottage(Plant plant);
 
+    public void updatePlantInPottage(Plant plant);
 
     public void removePlantInPottage(Plant plant);
 
---
+++ src/main/java/fr/mainguy/lebris/potager/service/impl/SquareServiceImpl.java
@@ -0,0 +1,33 @@
+package fr.mainguy.lebris.potager.service.impl;
+
+import fr.mainguy.lebris.potager.entity.Square;
+import fr.mainguy.lebris.potager.service.SquareService;
+
+import java.util.List;
+
+public class SquareServiceImpl implements SquareService {
+    @Override
+    public void createSquare() {
+
+    }
+
+    @Override
+    public void removeSquare() {
+
+    }
+
+    @Override
+    public void updateSquare() {
+
+    }
+
+    @Override
+    public void getSquare() {
+
+    }
+
+    @Override
+    public List<Square> getAllSquare() {
+        return List.of();
+    }
+}
